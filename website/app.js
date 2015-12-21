ef8d42eb87ce52be2d31494baa6a384d01750caf
jlissner <jlissner@gmail.com>
2015-12-20 19:39:10 -0600

    set port to 8081

--- website/app.js
+++ website/app.js
@@ -86,6 +86,6 @@ app.use(function(err, req, res, next) {
 console.log('~~~~ Starting Stellroute ~~~~');
 console.log('Current Environment: ' + app.get('env'));
 
-app.listen(80); 
+app.listen(8081); 
 
 module.exports = app;

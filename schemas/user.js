fa7d653fc499524273fb21a301d971bc429689b6
jlissner <jlissner@gmail.com>
2015-12-20 20:21:24 -0600

    update directory to see if this will fix it

--- schemas/user.js
+++ schemas/user.js
@@ -1,5 +1,7 @@
+const bycrpt = require('bcrypt-nodejs');
 const db = require('../config/aws');
 const duck = require('../modules/duck');
+
 
 var User =  new duck(db, 'Users', {
 	Item: {
@@ -10,16 +12,22 @@ var User =  new duck(db, 'Users', {
 		},
 		fb: {
 			email: String
-		},
-		name: {
-			first: String,
-			last: String
 		}
 	},
-	
 	HASH: 'Id',
-	HASHType: 'S'
-	//, Indexes : ['localEmail']
+	Indices : ['localEmail']
 });
+
+
+// methods ===============
+// generateing a hash
+User.methods.generateHash = function(password){
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+User.methods.validPassword = function(password){
+	return bcrypt.compareSync(password, this.local.password);
+};
 
 module.exports = User;

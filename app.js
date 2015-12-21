b04942873e826ee6022a60bc8715e5b277966c8a
jlissner <jlissner@gmail.com>
2015-12-20 20:21:24 -0600

    update directory to see if this will fix it

--- app.js
+++ app.js
@@ -3,18 +3,15 @@ const path             = require('path');
 const passport         = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const flash            = require('connect-flash');
+const setFlash         = require('./modules/setFlash');
 const favicon          = require('serve-favicon');
 const cookieParser     = require('cookie-parser');
 const bodyParser       = require('body-parser');
 const session          = require('express-session');
-const isLoggedIn       = require('./middleware/isLoggedIn');
-const setFlash         = require('./modules/setFlash');
 const routes           = require('./routes/index');
-const continents       = require('./routes/continents');
-const countries        = require('./routes/countries');
 const review           = require('./routes/review');
 const profile          = require('./routes/profile');
-const worldRegions     = require('./routes/world-regions');
+const db               = require('./config/aws'); 
 const fs               = require('fs');
 const app              = express();
 
@@ -30,10 +27,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); //Sets the public folder to be available available to the front end
 
 // Make the db accessible to our router
-/* app.use(function(req, res, next){
+app.use(function(req, res, next){
   req.db = db;
   next();
-}); */
+});
+
 
 // required for passport
 app.use(session({ 
@@ -47,25 +45,19 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 app.use(setFlash);
 
-//check to see if user is logged in on all pages except home directory
-app.use(isLoggedIn);
-
 //Set index.js to be the main router
 app.use('/', routes);
-app.use('/continents', continents);
-app.use('/countries', countries)
+app.use('/review', review);
 app.use('/profile', profile);
-app.use('/world-regions', worldRegions);
-//app.use('/review', review);
 
 // error handlers /////////////////////////////////////////////////////
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
-    console.error(req);
+    console.log(req);
     err.status = 404;
-    console.error('~~~~~~~~~~');
+    console.log('~~~~~~~~~~');
     next(err);
 });
 
@@ -93,5 +85,7 @@ app.use(function(err, req, res, next) {
 
 console.log('~~~~ Starting Stellroute ~~~~');
 console.log('Current Environment: ' + app.get('env'));
+
+app.listen(80); 
 
 module.exports = app;

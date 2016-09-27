21fa0d3747193c7845d2bcb6c1ae23fbc7279cac
plamming <peter@bagusco.com>
2016-09-27 14:06:26 -0500

    country schema

--- schemas/country.js
+++ schemas/country.js
@@ -4,57 +4,72 @@ const Country = Duck({
 	Table: 'Countries',
 	Item: {
 		Id: String,
-	//Required Fields Card
+	//Modal Fields Card
 		names: {
-			display: String, 
-			official: String,
-			native: Array,
-			alias: Array,
+			display: String,  
+			official: String, 
+			native: [{  
+				name: String, 
+				language: String,
+			}],
+			alias: Array, 
 		},
-		languages: Array,
 		abbreviation: String,
-		currency: String,
-		currenySmbol: String,
 		continent: Array,
 		worldRegions: Array,
 	//At a Glance Card
 		averageStay: String,
 		bestTimeToVisit: Array,
-		topPlaces: Array,
+		topPlaces: Array,	
 		tagline: String,
 		overview: String,
 	// Need to Know
 		needToKnow: [{
-			orderRanking: String,
-			postTitle: String,
+			order: Number,
+			category: String,
+				/*
+					communication
+				*/
+			title: String,
 			advice: String,
 		}],
+	//Quick Facts (or Fun Facts)
+		quickFact: [{
+			order: Number,
+			title: String,
+			explanation: String,
+		}],
 	//Travel Basics 
-		travelBasics: {
-			primaryAirports: [{
-				city: String,
-				airport: String,
-				abbreviation: String,
-			}],
-			entryTax: {
-				amount: String,
-				explanation: String,
-			}, 
-			exitTax: {
-				amount: String,
-				explanation: String,
-			}, 
-			timeZones: Array,
-			electricityType: String,
-			water: {
-				quality: String,
-				explanation: String,
-			},
-			toilets: {
-				types: String,
-				explanation: String,
-			},
+		airports: [{
+			city: String,
+			airport: String,
+			abbreviation: String,
+		}],
+		entryTax: {
+			amount: String,
+			explanation: String,
 		}, 
+		exitTax: {
+			amount: String,
+			explanation: String,
+		}, 
+		timeZones: Array,
+		electricity: {
+			type: String,
+			image: String,
+		};
+		mobile: {
+			simCard: bolean,
+			explanation: String,
+		};
+		water: {
+			quality: String,
+			explanation: String,
+		},
+		toilets: {
+			types: Array,
+			explanation: String,
+		},
 		visa: [{
 			countries: Array,
 			requirements: String,
@@ -65,14 +80,17 @@ const Country = Duck({
 			requirements: String,
 			explanation: String,
 		}],
-		atms: {
-			acceptedCards: Array,
-			availability: String,
-			explanation: String,
-		},
-		exchanging: {
-			title: String,
-			explanation: String,
+		money: {
+			overview: String,
+			currency: {
+				name: String,
+				symbol: String,
+			},
+			creditCards: {
+				overview: String,
+				availability: String,
+				accepted: Array,
+			}
 		},
 		clothing: [{
 			whatToWear: String,
@@ -80,6 +98,7 @@ const Country = Duck({
 		}],
 	// People and Customs
 		customs: {
+			overview: String,
 			tipping: {
 				tippingPractice: String,
 				explanation: String,
@@ -88,31 +107,25 @@ const Country = Duck({
 				bargainingStyle: String,
 				explanation: String,
 			}, 
-			dressStyle: {
-				dress: String,
+			dress: {
+				style: String,
 				explanation: String,
 			},
+			traditions: [{
+				title: String,
+				explanation: String,
+			}],
 		},
-		people: [{
-			practices: String,
-			explanation: String,
-		}],
-		traditions: [{
-			title: String,
-			explanation: String,
-		}],
-	//Quick Facts (or Fun Facts)
-		quickFact: [{
-					title: String,
-					explanation: String,
-				}],
-	//When to Go
-		whenToGo: {
-			months: Array,
+	// When to Go
+		whenToGo: [{
+			activity: String,
+			start: String,
+			end: String,
 			description: String,
-		},
+		}],
 		climate: String,
 		seasons: [{
+			order: Number,
 			season: String,
 			months: Array,
 			description: String,
@@ -123,62 +136,128 @@ const Country = Duck({
 			date: String,
 			description: String,
 		}], 
-	//Communication
-	//Getting Around
-		transitDescription: String,
-		suggestedTransit: {
-			mode: String,
-			description: String,
-		},
-		transitAdvice: [{
-				title: String,
+	// Communication
+		communication: {
+			overview: String,
+			languages: Array, 
+			basicSayings: [{
+				language: String,
+				saying: String,
+				translation: String,
 				explanation: String,
 			}],
-		car: {
+			expressions: [{
+				language: String,
+				phrase: String,
+				translation: String,
+				explanation: String,
+			}],
+			/*nonVerbal: [{
+				title: String,
+				explanation: String,
+			}], */
+		},
+	// Getting Around
+		transit: {
+			overview: String,
 			roadQuality: String,
-			tolls: String,
-			drivingStyle: String,
-			explanation: String,
-		},
-		taxis: {
-			alias: String,
-			bargainingStyle: String,
-			explanation: String,
-		},
-		train: {
-			trainQuality: String,
-			reliability: String,
-			explanation: String,
-		},
-		bus: {
-			operators: Array,
-			explanation: String,	
-		},
-		publicTransit: {
-			quality: String,
-			explanation: String,
-		},
-		bike: {
-			friendliness: String,
-			explanation: String,
+			suggested: [{
+				order: Number,
+				mode: String,
+				description: String,
+			}],
+			advice: [{
+				title: String,
+				description: String,
+			}],
+			public: {
+				quality: String,
+				title: String,
+				explanation: String,
+			},
+			car: {
+				roadQuality: String,
+				tolls: String,
+				drivingStyle: String,
+				title: String,
+				explanation: String,
+			},
+			taxis: {
+				alias: String,
+				bargaining: String,
+				explanation: String,
+			},
+			train: {
+				operators: Array,
+				trainQuality: String,
+				reliability: String,
+				title: String,
+				explanation: String,
+			},
+			bus: {
+				operators: Array,
+				title: String,
+				explanation: String,
+			},
 			
+			boats: {
+				title: String,
+				explanation: String,
+				
+			},
+			bike: {
+				friendliness: String,
+				title: String,
+				explanation: String,
+				
+			},
+			hitchHiking: {
+				title: String,
+				explanation: String, 
+			},
 		},
-		hitchHiking: {
-			title: String,
-			explanation: String, 
-		},
-
 	//Fascilities and Infrastructure 
-	// Eating and Meals 
-			eatingHabits: [{
+	// Eating and Meals
+		food: {
+			overview: String,
+			habits: [{
+				order: Number,
 				title: String,
 				explanation: String,
 			}],
+			dishs: [{
+				order: Number,
+				title: String,
+				mealType: String,
+				cuisinse: String,
+				explanation: String,
+			}],
+			drinks: [{
+				order: Number,
+				title: String,
+				drinkType: String,
+				explanation: String,
+				isAlcoholic: Boolean,
+			}],
+		}
 	//Top Places
-		//Top Destinations
-		//Top Attractions
-		//Geographical Landmarks 
-	//Regions
+		top: {
+			destinations: [{
+				order: String,
+				name: String,
+				type: String,
+			}],
+			attractions: [{
+				order: String,
+				name: String,
+				city: String,
+			}],
+			geographicalLandmarks: [{
+				order: String,
+				name: String,
+				classification: String,
+			}],
+		}
 	},
 	HASH: 'Id',
 	HASHType: 'S',

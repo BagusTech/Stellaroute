94aca1f381034491961c603c38f557480007ff3c
jlissner <jlissner@gmail.com>
2016-10-03 10:43:29 -0500

    updated flash to allow for more message types

--- modules/setFlash.js
+++ modules/setFlash.js
@@ -1,7 +1,9 @@
 const setFlash = function (req, res, next){
 	res.locals.flash = {
 		success: req.flash('success'),
-		error: req.flash('error')
+		error: req.flash('error'),
+		info: req.flash('info'),
+		warning: req.flash('warning'),
 	}
 
 	next();

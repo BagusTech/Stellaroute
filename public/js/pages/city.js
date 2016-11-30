371ae17254ffae11d1256910e0065d33fffcb0b1
jlissner <jlissner@gmail.com>
2016-11-29 19:38:53 -0600

    moving admin specific content out of city.js

--- public/js/pages/city.js
+++ public/js/pages/city.js
@@ -1,63 +1,7 @@
-/* global jQuery, duck */
+/* global jQuery */
 
-void function initCityFood($){
-	'use strict'
+void function initCity($){
+	'use strict';
 
-	function toggleUpdateFood(){
-		const $this = $(this);
-		const $wrapper = $this.closest('.content-card');
-
-		$wrapper.find('.js-food-item').toggleClass('hidden');
-		$wrapper.find('.js-update-food-item').toggleClass('hidden');
-		$wrapper.find('.js-edit-food-submit').toggleClass('hidden');
-
-		$this.toggleClass('btn-primary')
-			.toggleClass('btn-warning')
-			.text($this.text() === 'Edit' ? 'Cancel' : 'Edit');
-	}
-
-	function updateCityFood(currentItem, wrapper){
-		return () => {
-			const $inputs = $(wrapper).find('.js-food-field');
-			const updatedItem = {
-				Id: $('#CityId').val(),
-				food: {
-					foods: currentItem && currentItem.food && currentItem.food.foods || [],
-				},
-			}
-			const newFood = { Id: $(this).siblings('.js-food-id').val() || duck().uuid() }
-
-			$inputs.each((i, item) => {
-				const $item = $(item);
-
-				if($item.attr('data-summernote')){
-					newFood[$item.attr('name')] = $item.find('> .summernote').summernote('code');
-				} else if($item.attr('name') === 'dishes' && $item.val()){
-					newFood[$item.attr('name')] = $item.val().split(', ');
-				} else if($item.attr('type') === 'checkbox') {
-					if(newFood[$item.attr('name')] && $item.prop('checked')){
-						newFood[$item.attr('name')].push($item.val());
-					} else if($item.prop('checked')){
-						newFood[$item.attr('name')] = [$item.val()];
-					}
-				} else if($item.val()) {
-					newFood[$item.attr('name')] = $item.val();
-				}
-			});
-
-			updatedItem.food.foods.push(newFood);
-
-			duck('Cities').update(updatedItem, () => {location.reload();});
-		}
-	}
-
-	jQuery(() => {
-		duck('Cities').get({field: 'Id', value: $('#CityId').val(), findOne: true}, (data) => {
-			$('#AddFoodSubmit').click(updateCityFood(data, '#AddFoodForm'));
-			$('.js-edit-food').click(toggleUpdateFood);
-			$('.js-edit-food-submit').click(() => {
-				updateCityFood(data, $(this).closest('.content-card'))()
-			})
-		});
-	});
-}(jQuery)
+	jQuery(() => { $('.summernote').summernote({height: 150}); });
+}(jQuery);

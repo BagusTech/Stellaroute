bfb24c0dfa12ec993b950816bab325739bc9be55
jlissner <jlissner@gmail.com>
2017-01-03 20:33:23 -0600

    made the smtp pool

--- config/smtp.js
+++ config/smtp.js
@@ -5,6 +5,7 @@ const smtp = nodemailer.createTransport(smtpTransport({
 	host: 'email-smtp.us-west-2.amazonaws.com',
 	port: 465,
 	secure: true,
+	pool: true,
 	auth: {	
 		user: process.env.AWS_SMPT_ID,
 		pass: process.env.AWS_SMPT_KEY

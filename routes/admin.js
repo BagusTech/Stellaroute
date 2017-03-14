87540aeb391176c75c813bf25da36a8663a7076a
jlissner <jlissner@gmail.com>
2017-03-14 12:57:21 -0500

    add ability to add country region to a city, to add a new country region, and to filter by country region when looking at cities from a country view

--- routes/admin.js
+++ routes/admin.js
@@ -10,6 +10,7 @@ router.get('/', (req, res) => {
 		description: 'Stellaroute, founded in 2015, is the world\'s foremost innovator in travel technologies and services.',
 		users: pickTable('Users').cached(),
 		countries: pickTable('Countries').cached(),
+		countryRegions: pickTable('CountryRegions').cached(),
 		provinces: pickTable('Provinces').cached(),
 		cities: pickTable('Cities').join('country', pickTable('Countries').cached(), 'Id', 'url').items,
 	});
